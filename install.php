633e613b5266544706addddccc11c8b56e79b02a
Dmitry <zatulenko@gmail.com>
2017-08-21 10:45:59 +0300

    Создана папка медиа

--- install.php
+++ install.php
@@ -5,22 +5,27 @@ class com_bulletin_boardInstallerScript{
 	
 	// Constructor
 	
-	public function __construct(JAdapterInstance $adapter);
+	//public function __construct(JAdapterInstance $adapter);
 	
 	
-	//public function preflight($route, JAdapterInstance $adapter);
-	
-	
-	//public function postflight($route, JAdapterInstance $adapter);
-	
-	
-	public function install(JAdapterInstance $adapter){
-		echo "install extension";
+	public function preflight($route, JAdapterInstance $adapter){
+		echo JText::_('COM_BULLETIN_BOARD_PREFLIGHT_STR');
 	}
 	
+	public function postflight($route, JAdapterInstance $adapter){
+		echo JText::_('COM_BULLETIN_BOARD_POSTFLIGHT_STR');
+	}
 	
-	//public function update(JAdapterInstance $adapter);
+	public function install(JAdapterInstance $adapter){
+		echo JText::_('COM_BULLETIN_BOARD_INSTALL_STR');
+	}
 	
-	//public function uninstall(JAdapterInstance $adapter);
+	public function update(JAdapterInstance $adapter){
+		echo JText::_('COM_BULLETIN_BOARD_UPDATE_STR');
+	}
+	
+	public function uninstall(JAdapterInstance $adapter){
+		echo JText::_('COM_BULLETIN_BOARD_UNINSTALL_STR');
+	}
 }
 ?>
