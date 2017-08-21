a75eaf838755ea740948dd26813d50526af0c938
Dmitry <zatulenko@gmail.com>
2017-08-21 09:55:32 +0300

    Изменён файл install

--- install.php
+++ install.php
@@ -1,1 +1,26 @@
-<?php ?>
+<?php 
+defined('_JEXEC') or die;
+
+class com_bulletin_boardInstallerScript{
+	
+	// Constructor
+	
+	public function __construct(JAdapterInstance $adapter);
+	
+	
+	//public function preflight($route, JAdapterInstance $adapter);
+	
+	
+	//public function postflight($route, JAdapterInstance $adapter);
+	
+	
+	public function install(JAdapterInstance $adapter){
+		echo "install extension";
+	}
+	
+	
+	//public function update(JAdapterInstance $adapter);
+	
+	//public function uninstall(JAdapterInstance $adapter);
+}
+?>
